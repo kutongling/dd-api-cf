7d96d859f529c9deb42625fb691830ddc9753091
kutongling <ku_tong_ling@qq.com>
2025-01-27 08:10:09 +0800

    0

--- cloudflare-api-proxy/src/utils/signature.ts
+++
@@ -1,6 +0,0 @@
-import crypto from 'crypto';
-
-export function generateSignature(appId: string, appSecret: string, timestamp: number, path: string): string {
-    const data = appId + timestamp + path + appSecret;
-    return crypto.createHash('sha256').update(data).digest('base64');
-}
--- src/utils/signature.ts
+++
@@ -1,7 +0,0 @@
-export async function generateSignature(appId: string, timestamp: number, path: string, appSecret: string): Promise<string> {
-  const encoder = new TextEncoder();
-  const data = `${appId}${timestamp}${path}${appSecret}`;
-  
-  const hashBuffer = await crypto.subtle.digest('SHA-256', encoder.encode(data));
-  return btoa(String.fromCharCode(...new Uint8Array(hashBuffer)));
-}
